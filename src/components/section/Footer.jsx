ecd2b876383b4dc3b936e633d581605b1df34c6b
홍수민 <ghdtnals1@soominui-MacBookPro.local>
2023-11-24 19:20:51 +0900

    23.11.24-03

---
+++ src/components/section/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer id='footer' role='content_info'>
+            footer
+        </footer>
+    )
+}
+
+export default Footer
