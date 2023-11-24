4fd40ac668f75881ecdb5d8b04ee9f747775ccec
홍수민 <ghdtnals1@soominui-MacBookPro.local>
2023-11-24 22:12:15 +0900

    23.11.24-05-1

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -2,22 +2,15 @@ import React from 'react'
 
 import Menu from '../header/Menu';
 import Sns from '../header/Sns';
+import Logo from '../header/Logo';
 
 const Header = () => {
 
     return (
         <header id='header' role='banner'>
-                <h1 className='header__logo'>
-                    <a href='/'>
-                        <em aria-hidden='true'></em>
-                        <span>portfolio<br />youtube</span>
-                    </a>
-                </h1>
-
+                <Logo />
                 <Menu />
                 <Sns />
-            
-
         </header>
     )
 }
