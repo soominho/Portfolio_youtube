2910ea832502a37f89e8005cf3d59b52116e8129
홍수민 <ghdtnals1@soominui-MacBookPro.local>
2023-11-25 15:09:59 +0900

    23.11.25-01

--- src/pages/Website.jsx
+++ src/pages/Website.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
 import Main from '../components/section/Main'
 
 const Website = () => {
     return (
-        <Main>Website</Main>
+        <Main 
+            title = "웹표준 사이트"
+            description="웹표준 사이트 튜토리얼 강의입니다.">
+            Website
+        </Main>
     )
 }
-    
+
 export default Website
