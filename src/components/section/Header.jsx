4a9adeeddf8f5b53f3049c8d9ff2ae062dfa6b88
홍수민 <ghdtnals1@soominui-MacBookPro.local>
2023-11-27 16:26:31 +0900

    23.11.27-01

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Menu from '../header/Menu';
 import Sns from '../header/Sns';
 import Logo from '../header/Logo';
 
 const Header = () => {
+    const [isMenuActive, setIsMenuActive] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuActive(!isMenuActive);
+    }
 
     return (
-        <header id='header' role='banner'>
-                <Logo />
+        <header id='header' role='banner' className={isMenuActive ? 'active' : ''}>
+                <Logo toggleMenu={toggleMenu} />
                 <Menu />
                 <Sns />
         </header>
--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -4,11 +4,13 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import Header from './Header'
 import Footer from './Footer'
 import Search from './Search'
+import ScrollTo from '../../utils/ScrollTo'
 
 
 const Main = (props) => {
     return (
         <HelmetProvider>
+            <ScrollTo />
             <Helmet titleTemplate="%s | Portfolio Youtube" 
                 defaultTitle="Portfolio Youtube" 
                 defer={false}>
